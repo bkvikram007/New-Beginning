78770d72130085e783af330d4a69729a43b8ac21
bkvikram007 <167081150+bkvikram007@users.noreply.github.com>
2024-04-22 10:01:24 +0530

    Binary search in python

---
+++ Binarysearch.py
@@ -0,0 +1,26 @@
+#Binary Search 
+nums = [12,34,2,56,90,33,89,120,20,25,19]
+nums = sorted(nums)
+
+target = 34 
+#target = 97
+
+left = 0 
+right  = len(nums) - 1
+found = False
+
+while left<=right:
+    mid = (left+right)//2
+    if nums[mid] == target:
+        found = True
+        break
+    elif mid > target :
+        right = mid -1
+    else:
+        left = mid +1
+    
+
+if found==True : 
+    print("Element is present in list")
+else:
+    print("Element is not present")
